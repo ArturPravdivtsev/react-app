bc93d50448d16c2241f565a6c9218edf209e9277
Artur <artur.pravdivtsev@gmail.com>
2021-08-04 22:17:54 +0300

    added middleware

--- src/store/actions/messages.js
+++ src/store/actions/messages.js
@@ -1,9 +1,28 @@
+import { AUTHORS } from '../../constants'
+
 export const ADD_MESSAGE = 'MESSAGES:ADD_MESSAGE'
 
-export const addMessage = (chatId, message) => ({
+export const addMessage = (chatId, message, author) => ({
     type: ADD_MESSAGE,
     payload: {
         chatId,
         message,
+        author
     },
 })
+
+export const sendMessageFromBot = (chatId, message) => {
+    return (dispatch, getState) => {
+        dispatch(addMessage(chatId, message))
+
+        setTimeout(() => {
+            dispatch(
+                addMessage(chatId, {
+                    id: `message${Date.now()}`,
+                    author: AUTHORS.BOT,
+                    text: 'Привет, я - бот!',
+                })
+            )
+        }, 1500)
+    }
+}
